c7e50369e94b7c48a48b4279556598673fc4770a
TakuyaAshikawa <mk-ae.rech@hotmail.co.jp>
2019-12-23 20:47:59 +0000

    Finish expense-item-setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191223193921) do
+ActiveRecord::Schema.define(version: 20191223202618) do
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "revenues", force: :cascade do |t|
     t.string "name", null: false
